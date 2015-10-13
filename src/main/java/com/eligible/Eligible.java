f76885ff0191f4d9e09e632f7985fd2219aecadb
Ankit Dimania <ankit.dimania@eligibleapi.com>
2015-10-13 20:52:59 +0530

    Increase version to 1.3.0

--- src/main/java/com/eligible/Eligible.java
+++ src/main/java/com/eligible/Eligible.java
@@ -29,7 +29,7 @@ public abstract class Eligible {
     /**
      * Eligible Java Binding version.
      */
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
 
     /**
      * Eligible API key for authentication.
