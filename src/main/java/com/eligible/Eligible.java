3344c953f9ef63d34b2fd149e1b09275b1ee0371
Ankit Dimania <ankitdimania@eligible.com>
2016-06-06 20:28:49 +0530

    Change eligible version to v1.7.0

--- src/main/java/com/eligible/Eligible.java
+++ src/main/java/com/eligible/Eligible.java
@@ -30,7 +30,7 @@ public abstract class Eligible {
     /**
      * Eligible Java Binding version.
      */
-    public static final String VERSION = "1.6.0";
+    public static final String VERSION = "1.7.0";
 
     /**
      * Eligible API key for authentication.
